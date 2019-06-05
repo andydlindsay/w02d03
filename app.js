8a12cab07d5152002d7abfeaeba64268017712a4
Andy Lindsay <andydlindsay@gmail.com>
2019-06-05 09:31:34 -0700

    added morgan logger

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 
 // set view engine to ejs
 app.set('view engine', 'ejs');
+
+// set up the logger
+app.use(morgan('dev'));
 
 app.get('/dogs', (request, response) => {
     response.render('index');
